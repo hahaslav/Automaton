d4bb434b535952334a86f6efb654f1c0c741038f
hahaslav <yara-2004@ukr.net>
2024-05-10 21:19:38 +0300

    Add read from file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+std::string read_file(std::string filename = "input.txt") {
+    std::ifstream fin(filename);
+    std::stringstream buffer;
+    buffer << fin.rdbuf();
+    fin.close();
+
+    return buffer.str();
+}
+
+int main(int argc, char *argv[]) {
+    std::string text;
+    if (argc > 1) {
+        text = read_file(argv[1]);
+    } else {
+        text = read_file();
+    }
+
+    std::cout << text;
     return 0;
 }
